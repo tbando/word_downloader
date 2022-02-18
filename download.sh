1e301d541dba3f22c775a73dbb690554ccd2ecf8
tbando <bandy773@gmail.com>
2022-02-18 12:36:49 +0900

    update

--- download.sh
+++ download.sh
@@ -9,9 +9,19 @@ DIR="./download"
 while read line
 do
 	echo "$line"
+	# US
+	MP3_PATH=''
 	MP3_PATH=$(curl -s "https://dictionary.cambridge.org/dictionary/english/${line}" | grep -o -e '[^"]*us_pron.*mp3[^"]*' | head -n1)
 	if [[ "${MP3_PATH}" != "" ]]
 	then
-	  wget "${URI_BASE}${MP3_PATH}" -O "${DIR}/${line}.mp3"
+	  wget "${URI_BASE}${MP3_PATH}" -O "${DIR}/${line}_us.mp3"
+	fi
+
+	# UK
+	MP3_PATH=''
+	MP3_PATH=$(curl -s "https://dictionary.cambridge.org/dictionary/english/${line}" | grep -o -e '[^"]*uk_pron.*mp3[^"]*' | head -n1)
+	if [[ "${MP3_PATH}" != "" ]]
+	then
+	  wget "${URI_BASE}${MP3_PATH}" -O "${DIR}/${line}_uk.mp3"
 	fi
 done < "${WORD_LIST}"
